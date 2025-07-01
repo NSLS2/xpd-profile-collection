413c019b00c0ae9195ce0bd961bd7385df4a8ab2
kari Barry <kezzsim@gmail.com>
2025-07-01 16:02:57 -0400

    🔑 Add standard DE data security accoutrements

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -2,7 +2,11 @@
 import nslsii
 import ophyd.signal
 import logging
+import os
+import time as ttime
 from IPython import get_ipython
+from databroker import Broker
+from tiled.client import from_profile
 
 ip = get_ipython()
 
@@ -17,9 +21,43 @@ ophyd.signal.EpicsSignal.set_defaults(connection_timeout=5)
 # this command takes away much of the boilerplate for settting up a profile
 # (such as setting up best effort callbacks etc)
 
-nslsii.configure_base(get_ipython().user_ns, 'xpd', pbar=True, bec=True,
-                      magics=True, mpl=True, epics_context=False,
-                      publish_documents_with_kafka=True)
+
+class TiledInserter:
+    def insert(self, name, doc):
+        ATTEMPTS = 20
+        error = None
+        for attempt in range(ATTEMPTS):
+            try:
+                tiled_writing_client.post_document(name, doc)
+            except Exception as exc:
+                print("Document saving failure:", repr(exc))
+                error = exc
+            else:
+                break
+            ttime.sleep(2)
+        else:
+            # Out of attempts
+            raise error
+
+
+# Define tiled catalog
+tiled_writing_client = from_profile(
+    "nsls2", api_key=os.environ["TILED_BLUESKY_WRITING_API_KEY_TLA"]
+)["xpd"]["raw"]
+tiled_inserter = TiledInserter()
+c = tiled_reading_client = from_profile("nsls2")["xpd"]["raw"]
+db = Broker(c)
+
+nslsii.configure_base(
+    get_ipython().user_ns,
+    tiled_inserter,
+    pbar=True,
+    bec=True,
+    magics=True,
+    mpl=True,
+    epics_context=False,
+    publish_documents_with_kafka=True,
+)
 
 del one_1d_step
 del one_nd_step
@@ -28,6 +66,7 @@ del one_shot
 # At the end of every run, verify that files were saved and
 # print a confirmation message.
 from bluesky.callbacks.broker import verify_files_saved, post_run
+
 # RE.subscribe(post_run(verify_files_saved, db), 'stop')
 
 # Uncomment the following lines to turn on verbose messages for
@@ -37,9 +76,9 @@ from bluesky.callbacks.broker import verify_files_saved, post_run
 # logging.basicConfig(level=logging.DEBUG)
 
 
-RE.md['facility'] = 'NSLS-II'
-RE.md['group'] = 'XPD'
-RE.md['beamline_id'] = '28-ID-2'
+RE.md["facility"] = "NSLS-II"
+RE.md["group"] = "XPD"
+RE.md["beamline_id"] = "28-ID-2"
 
 import subprocess
 
@@ -49,6 +88,40 @@ def show_env():
     # the path via activate
     proc = subprocess.Popen(["conda", "list"], stdout=subprocess.PIPE)
     out, err = proc.communicate()
-    a = out.decode('utf-8')
-    b = a.split('\n')
-    print(b[0].split('/')[-1][:-1])
+    a = out.decode("utf-8")
+    b = a.split("\n")
+    print(b[0].split("/")[-1][:-1])
+
+
+# Post-data security helper functions
+# check the current logged in + active user
+def whoami():
+    try:
+        print(f"\nLogged in to Tiled as: {c.context.whoami()['identities'][0]['id']}\n")
+    except TypeError as e:
+        print("Not authenticated with Tiled! Please login...")
+
+
+whoami()
+
+
+# logout of Tiled and clear the cached default identities (username)
+def logout():
+    c.logout(clear_default=True)
+
+
+# login to Tiled using the specified username
+# this version automatically logs-out the existing user when called
+def login():
+    beamline_username = input("Please enter your username: ")
+    beamline_unauthenticated = (
+        c.context.api_key is None and c.context.http_client.auth is None
+    )
+    if not beamline_unauthenticated:
+        beamline_current_user = c.context.whoami()["identities"][0]["id"]
+        if beamline_username != beamline_current_user:
+            logout()
+
+    c.login(username=beamline_username)
+
+    print(f"Logged in to Tiled as {beamline_username}!")
