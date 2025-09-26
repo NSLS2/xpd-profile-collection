c5f22aa78a7b7344eca82f9745af87fb04c89617
Max Rakitin <mrakitin@bnl.gov>
2025-09-26 16:45:35 -0400

    Add comments about purposes of ZMQ subscriptions to RE/xrun

--- startup/971-zmq.py
+++ startup/971-zmq.py
@@ -1,4 +1,4 @@
 from bluesky.callbacks.zmq import Publisher
 
-raw_publisher = Publisher(glbl['inbound_proxy_address'], RE=xrun, prefix=b'raw')
-raw_publisher = Publisher(glbl['inbound_proxy_address'], RE=RE, prefix=b'raw')
+raw_publisher = Publisher(glbl['inbound_proxy_address'], RE=xrun, prefix=b'raw')  # used in bsui
+raw_publisher = Publisher(glbl['inbound_proxy_address'], RE=RE, prefix=b'raw')  # used by bluesky-queueserver
