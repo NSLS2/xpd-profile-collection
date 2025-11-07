707bd8aefe5b8e1cc2410d0e72682236c0bab5e5
AJ Sligar <asligar@bnl.gov>
2025-11-07 17:31:36 -0500

    added is_re_worker_active to 971-zmq.py

--- startup/971-zmq.py
+++ startup/971-zmq.py
@@ -1,4 +1,7 @@
 from bluesky.callbacks.zmq import Publisher
 
-raw_publisher = Publisher(glbl['inbound_proxy_address'], RE=xrun, prefix=b'raw')  # used in bsui
-raw_publisher = Publisher(glbl['inbound_proxy_address'], RE=RE, prefix=b'raw')  # used by bluesky-queueserver
+if is_re_worker_active():  # running in queueserver
+    raw_publisher = Publisher(glbl['inbound_proxy_address'], RE=RE, prefix=b'raw')  # used by bluesky-queueserver
+
+else:
+    raw_publisher = Publisher(glbl['inbound_proxy_address'], RE=xrun, prefix=b'raw')  # used in bsui
