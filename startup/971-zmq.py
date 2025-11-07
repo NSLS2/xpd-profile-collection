2c7f1e4e32a5f00d809aeff4f5636a3b636c5246
XPD Operator <xf28id2@xf28id2-ws3.nsls2.bnl.local>
2025-11-07 17:51:35 -0500

    removed admin in user_group_permission.yaml

--- startup/971-zmq.py
+++ startup/971-zmq.py
@@ -3,5 +3,5 @@ from bluesky.callbacks.zmq import Publisher
 if is_re_worker_active():  # running in queueserver
     raw_publisher = Publisher(glbl['inbound_proxy_address'], RE=RE, prefix=b'raw')  # used by bluesky-queueserver
 
-else:
+else: # running in bsui
     raw_publisher = Publisher(glbl['inbound_proxy_address'], RE=xrun, prefix=b'raw')  # used in bsui
